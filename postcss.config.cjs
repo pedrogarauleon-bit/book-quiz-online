1483f14551927eb1c9e645e9acb8bf8e3edb211f
pedrogarauleon-bit <pedrogarauleon@gmail.com>
2026-08-30 14:14:44 +0200

    Add configuration files: app.postcss, svelte.config.js, vite.config.js, tailwind.config.cjs, postcss.config.cjs

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -3,11 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 const base = process.env.VITE_BASE ?? "/"
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
   base,
-  optimizeDeps: {
-    include: ['lodash.get', 'lodash.isequal', 'lodash.clonedeep']
-  }
 })
