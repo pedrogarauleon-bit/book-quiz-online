0f10327c52a52cd553842237ab3afb21b5c4dec3
pedrogarauleon-bit <pedrogarauleon@gmail.com>
2026-08-30 13:09:38 +0200

    Fix quiz state handling and scoring

--- src/lib/QuestionSystem.svelte
+++ src/lib/QuestionSystem.svelte
@@ -1,112 +1,69 @@
 <script>
-  import Carousel from "svelte-carousel";
-  import Question from "./Question.svelte";
-  import { store, initStore } from "../assets/store.js";
-  import { fade } from "svelte/transition";
+  import Carousel from 'svelte-carousel';
+  import Question from './Question.svelte';
+  import { store } from '../assets/store.js';
+  import NatureDecriptor from './NatureDecriptor.svelte';
+  import ResultScreen from './ResultScreen.svelte';
+  import { onMount } from 'svelte';
+  import * as utils from '../assets/utils.js';
 
-  import * as utils from "../assets/utils.js";
-  import { onMount } from "svelte";
-  import NatureDecriptor from "./NatureDecriptor.svelte";
-  import ResultScreen from "./ResultScreen.svelte";
-
-  let data = $store.questions;
-  let numberOfQuestions = $store.numQuestions;
+  let data = [];
+  let numberOfQuestions = 10;
   let questions = [];
   let index = 0;
   export let dataExt = null;
-
   let carousel = null;
 
-  function onCorrectQuestion(event) {
-    carousel.goToNext();
-    index++;
-
-    // if (index === numberOfQuestions) {
-    //   console.log("Finished");
-    // }
+  function getRandomNumOfQuestions(count) {
+    const result = [];
+    while (result.length < count) {
+      const q = data[Math.floor(Math.random() * data.length)];
+      if (q && !result.includes(q)) result.push(q);
+    }
+    return result;
   }
 
-  function getRandomNumOfQuestions(numberOfQuestions) {
-    let questions = [];
+  function onCorrectQuestion() {
+    carousel?.goToNext();
+    index++;
+    updateData();
+  }
 
-    while (questions.length < numberOfQuestions) {
-      let randomIndex = Math.floor(Math.random() * data.length);
-      let randomQuestion = data[randomIndex];
-
-      if (!questions.includes(randomQuestion)) {
-        questions.push(randomQuestion);
-      }
+  function updateData() {
+    if (!$store.points || !$store.maxPoints) return;
+    const values = {};
+    for (const [key, value] of Object.entries($store.points)) {
+      values[key] = $store.maxPoints[key] ? value / $store.maxPoints[key] : 0;
     }
-
-    return questions;
+    $store.weightedPoints = values;
   }
 
   onMount(() => {
-    // initStore(dataExt);
     $store.natures = dataExt.natures;
     $store.questions = dataExt.questions;
     $store.natureToBooks = dataExt.natureToBooks;
     $store.natureDescription = dataExt.natureDescription;
     $store.strings = dataExt.strings;
     $store.booksData = dataExt.booksData;
-
-    const point = dataExt.natures.reduce((acc, cur) => {
-    acc[cur] = 0;
-    return acc;
-  }, {});
-
-    $store.points = point;
-    $store.maxPoints = utils.getMaxPoints(point, $store.questions);
+    $store.points = dataExt.natures.reduce((acc, cur) => ({ ...acc, [cur]: 0 }), {});
+    $store.maxPoints = utils.getMaxPoints($store.points, $store.questions);
+    updateData();
 
     data = $store.questions;
     numberOfQuestions = $store.numQuestions;
-
-    if (numberOfQuestions > 0) {
-      questions = getRandomNumOfQuestions(numberOfQuestions);
-    } else {
-      numberOfQuestions = data.length;
-      questions = data;
-    }
-
-    store.subscribe((value) => {
-      updateData();
-    });
+    questions = numberOfQuestions > 0 ? getRandomNumOfQuestions(numberOfQuestions) : data;
   });
-
-  function updateData() {
-    let values = {};
-
-    for (let [key, value] of Object.entries($store.points)) {
-      values[key] = value / $store.maxPoints[key];
-    }
-
-    store.weightedPoints = values;
-  }
-
-  function onPageChange(event) {}
-
-  let natureDescriptionFinished = false;
-  let natureDecriptor = null;
 </script>
 
 {#if index !== numberOfQuestions}
-  <Carousel
-    bind:this="{carousel}"
-    infinite="{false}"
-    dots="{false}"
-    arrows="{false}"
-    swiping="{false}"
-    on:pageChange="{(event) => onPageChange(event)}">
+  <Carousel bind:this={carousel} infinite={false} dots={false} arrows={false} swiping={false}>
     {#each questions as question (question.id)}
-      <Question
-        on:correctQuestion="{onCorrectQuestion}"
-        questionData="{data[question.id]}" />
+      <Question on:correctQuestion={onCorrectQuestion} questionData={question} />
     {/each}
   </Carousel>
-{:else if !natureDescriptionFinished}
-  <NatureDecriptor
-    bind:this="{natureDecriptor}"
-    bind:finish="{natureDescriptionFinished}" />
 {:else}
-  <ResultScreen />
+  <NatureDecriptor bind:finish={natureDescriptionFinished} />
+  {#if natureDescriptionFinished}
+    <ResultScreen />
+  {/if}
 {/if}
